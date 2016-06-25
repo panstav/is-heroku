918e16cfd6716fc19f8539420c4079ba689d4d4a
Stav Geffen <stavgeffen@gmail.com>
2016-06-25 13:54:52 +0300

    Added remote test

--- test/index.js
+++ test/index.js
@@ -1,11 +1,23 @@
 var isHeroku = require('../');
 
 var expect = require('expect.js');
+var got = require('got');
 
 describe('isHeroku', function(){
 
 	it('Should return false locally', function(){
 		expect(isHeroku()).to.not.be.ok();
+	});
+
+	it('Should return true on a Heroku instance', function(){
+
+		// source for that instance: https://github.com/panstav/is-heroku-remote-test
+
+		return got('https://is-heroku-remote-test.herokuapp.com/', { json: true }).then(function(res){
+			expect(res.body).to.have.key('result');
+			expect(res.body.result).to.be.equal(true);
+		});
+
 	});
 
 });
